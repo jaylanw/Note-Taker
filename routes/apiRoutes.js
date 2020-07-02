7ced3b5933c85326c2ff498679d94b9532c05a7b
Jaylan <jaylanwienckowski@gmail.com>
2020-07-02 09:28:59 -0500

    adding api routes. first local run

---
+++ routes/apiRoutes.js
@@ -0,0 +1,17 @@
+var noteData = require("../Develop/db/db.json");
+
+module.exports = function (app) {
+
+  app.get("/api/notes", function (req, res) {
+    res.json(noteData);
+  });
+
+  app.post("/api/notes", function (req, res) {
+    noteData.push(req.body);
+    res.json("Your Note Was Saved!");
+  });
+
+  // app.delete("/api/notes/:id", function (req, res) {
+  // });
+
+}
